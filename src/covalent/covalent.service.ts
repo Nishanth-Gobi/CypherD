7fb6a808ef491662faa03a877b65406cd581403c
Nishanth <nish.professional@gmail.com>
2023-01-06 11:33:33 +0530

    using promises

--- src/covalent/covalent.service.ts
+++ src/covalent/covalent.service.ts
@@ -22,7 +22,6 @@ export class CovalentService {
         authorization: 'Access-Control-Allow-Origin',
       },
     };
-
     let total_balance = 0;
     const balanceResponse = {
       updated_at: '',
@@ -30,6 +29,7 @@ export class CovalentService {
       total_balance: 0,
     };
 
+    const promises = [];
     for (let i = 0; i < chains.length; i++) {
       const api_endpoint =
         'https://api.covalenthq.com/v1/' +
@@ -39,27 +39,35 @@ export class CovalentService {
         '/balances_v2/?key=' +
         api_key;
 
-      const { data } = await axios.get(api_endpoint, config);
-
-      for (const object of get(data, 'data.items', [])) {
-        const balance = object.balance / Math.pow(10, object.contract_decimals);
-        const holding = balance * object.quote_rate;
-
-        const item = {};
-        set(item, 'name', object.contract_name);
-        set(item, 'ticker', object.contract_ticker_symbol);
-        set(item, 'balance', balance);
-        set(item, 'logo_url', object.logo_url);
-        set(item, 'holding', holding);
-        set(item, 'is_verified', object.quote_rate != null);
-        set(item, 'chain_id', get(data, 'data.chain_id', 0));
-
-        total_balance += holding;
-        balanceResponse.balances?.push(item);
-      }
-      set(balanceResponse, 'updated_at', get(data, 'data.updated_at', ''));
+      promises.push(axios.get(api_endpoint, config));
     }
-    set(balanceResponse, 'total_balance', total_balance);
+    const settledPromises = await Promise.allSettled(promises);
+
+    settledPromises.forEach((promise) => {
+      const { status } = promise;
+      if (status === 'fulfilled') {
+        // chainsData.push(promise.value);
+        const { data } = promise.value;
+        for (const object of get(data, 'data.items', [])) {
+          const balance =
+            object.balance / Math.pow(10, object.contract_decimals);
+          const holding = balance * object.quote_rate;
+          const item = {};
+          set(item, 'name', object.contract_name);
+          set(item, 'ticker', object.contract_ticker_symbol);
+          set(item, 'balance', balance);
+          set(item, 'logo_url', object.logo_url);
+          set(item, 'holding', holding);
+          set(item, 'is_verified', object.quote_rate != null);
+          set(item, 'chain_id', get(data, 'data.chain_id', 0));
+
+          total_balance += holding;
+          balanceResponse.balances?.push(item);
+        }
+        set(balanceResponse, 'updated_at', get(data, 'data.updated_at', ''));
+        set(balanceResponse, 'total_balance', total_balance);
+      }
+    });
 
     return balanceResponse;
   }
